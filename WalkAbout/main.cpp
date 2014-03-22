1ff6ad0199b9dcaf633d8c5b041b98886e4adefe
John Donovan <jdonovan@syr.edu>
2014-03-22 11:27:38 -0400

    Initial Commit

---
+++ WalkAbout/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  WalkAbout
+//
+//  Created by John Donovan on 3/22/14.
+//  Copyright (c) 2014 John Donovan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
